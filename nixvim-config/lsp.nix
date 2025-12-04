96e1bdf7f4b309c02097fa45be66499bbb0e749a
lukask <lukas.kretschmer@igd-r.fraunhofer.de>
2025-12-04 15:23:07 +0100

    more boarders

--- nixvim-config/lsp.nix
+++ nixvim-config/lsp.nix
@@ -135,4 +135,22 @@
     #  };
     #};
   };
+
+  extraConfigLua = ''
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+      vim.lsp.handlers.hover, {
+        border = "rounded"
+      }
+    )
+    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+      vim.lsp.handlers.signature_help, {
+        border = "rounded"
+      }
+    )
+    vim.diagnostic.config({
+      float = {
+        border = "rounded"
+      }
+    })
+  '';
 }
