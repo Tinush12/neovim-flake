c1007640fa33a6e7a02fd378614b6f8509e13bf6
Lukas <lukaskretschmer@mailbox.org>
2025-12-08 23:55:53 +0100

    theming

--- nixvim-config/plugins.nix
+++ nixvim-config/plugins.nix
@@ -13,14 +13,116 @@
     bufferline = {
       # Tab line a the Top
       enable = true;
-      settings = {options = {always_show_bufferline = false;};};
+      settings = {
+        options = {
+          always_show_bufferline = true;
+          separator_style = "slope"; #slant or "slope", "thick", "thin"
+          indicator = {
+            icon = "▎";
+            style = "icon";
+          };
+          diagnostics = "nvim_lsp";
+          mode = "buffers";
+
+          close_icon = " ";
+          buffer_close_icon = "󰱝 ";
+          modified_icon = "󰔯 ";
+
+          offsets = [
+            {
+              highlight = "Directory";
+              text_align = "left";
+            }
+          ];
+        };
+      };
     };
+
     lualine = {
       # Status line at the Bottom
       enable = true;
-      settings.options = {
-        globalstatus = true;
-        theme = "nord";
+      settings = {
+        options = {
+          globalstatus = true;
+          section_separators = {
+            left = "";
+            right = "";
+          };
+          component_separators = {
+            left = "";
+            right = "";
+          };
+          extensions = [
+            "fzf"
+          ];
+          disabledFiletypes = {
+            statusline = [
+              "alpha"
+            ];
+          };
+        };
+        sections = {
+          lualine_a = [
+            {
+              __unkeyed-1 = "mode";
+              icon = "";
+            }
+          ];
+          lualine_b = [
+            {
+              __unkeyed-1 = "branch";
+              icon = "";
+            }
+            {
+              __unkeyed-1 = "diff";
+              symbols = {
+                added = " ";
+                modified = " ";
+                removed = " ";
+              };
+            }
+          ];
+          lualine_c = [
+            {
+              __unkeyed-1 = "diagnostics";
+              sources = ["nvim_lsp"];
+              symbols = {
+                error = " ";
+                warn = " ";
+                info = " ";
+                hint = "󰝶 ";
+              };
+            }
+            {
+              __unkeyed-1 = "navic";
+            }
+          ];
+          lualine_x = [
+            {
+              __unkeyed-1 = "filetype";
+              icon_only = true;
+              separator = "";
+              padding = {
+                left = 1;
+                right = 0;
+              };
+            }
+            {
+              __unkeyed-1 = "filename";
+              path = 1;
+            }
+          ];
+          lualine_y = [
+            {
+              __unkeyed-1 = "progress";
+            }
+          ];
+          lualine_z = [
+            {
+              __unkeyed-1 = "location";
+            }
+          ];
+        };
       };
     };
 
--- nixvim-config/theme.nix
+++ nixvim-config/theme.nix
@@ -8,19 +8,50 @@
         transparent = true;
       };
     };
+    kanagawa-paper = {
+      enable = false;
+      autoLoad = true;
+      settings = {
+        integrations = {
+          treesitter = true;
+          telescope = true;
+          which_key = true;
+        };
+        theme = "ink";
+        transparent = false;
+        styles = {
+          comments = {
+            italic = true;
+          };
+          functions = {
+            italic = true;
+          };
+          keywords = {
+            italic = true;
+          };
+          statement_style = {
+            bold = true;
+          };
+          terminal_colors = false;
+        };
+      };
+    };
     kanagawa = {
       enable = true;
       settings = {
         colors = {
           theme = {
-            all.ui.bg_gutter = "none";
+            all.ui = {
+              bg_gutter = "none";
+              #bg = "#0a1216";
+            };
             float.bg = "none";
           };
           wave.ui.float.bg = "none";
         };
         commentStyle.italic = true;
         theme = "wave"; # wave lotus
-        transparent = true;
+        transparent = false;
       };
     };
   };
