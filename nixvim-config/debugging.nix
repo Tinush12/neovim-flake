be6adb8066a71a0462a347938b34afcd45634dbc
Lukas <lukaskretschmer@mailbox.org>
2026-09-01 16:56:04 +0200

    updated debugger

--- nixvim-config/debugging.nix
+++ nixvim-config/debugging.nix
@@ -23,14 +23,15 @@
           };
         };
       };
-      # Uncommmend to use other debugger
+      # Uncommment to use other debugger
 
-      executables = {
-        gdb = {
-          command = "${pkgs.gdb}/bin/gdb";
-          args = ["-i" "dap"];
-        };
-      };
+      # Uncommment gdb for g++
+      #executables = {
+      #  gdb = {
+      #    command = "${pkgs.gdb}/bin/gdb";
+      #    args = ["-i" "dap"];
+      #  };
+      #};
       #executables = {
       #  cppdbg = {
       #    command = "${pkgs.vscode-extensions.ms-vscode.cpptools}/share/vscode/extensions/ms-vscode.cpptools/debugAdapters/bin/OpenDebugAD7";
@@ -55,21 +56,22 @@
           '';
           cwd = "\${workspaceFolder}";
         }
-        # Uncommmend to use other debugger
+        # Uncommment to use other debugger
 
-        {
-          name = "GDB";
-          type = "gdb";
-          request = "launch";
-          program.__raw = ''
-            function()
-              return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-            end
-          '';
-          cwd = "\${workspaceFolder}";
-          stopAtEntry = true;
-          args = [];
-        }
+        # Uncommment gdb for g++
+        #{
+        #  name = "GDB";
+        #  type = "gdb";
+        #  request = "launch";
+        #  program.__raw = ''
+        #    function()
+        #      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        #    end
+        #  '';
+        #  cwd = "\${workspaceFolder}";
+        #  stopAtEntry = true;
+        #  args = [];
+        #}
         #{
         #  name = "LLDB Native";
         #  type = "lldb";
